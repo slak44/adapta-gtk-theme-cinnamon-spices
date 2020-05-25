8c7baf2386cb7ce28dfa202d9abe97414c32a76a
steve <29017677+smurphos@users.noreply.github.com>
2020-05-25 07:32:30 +0100

    Cinnamon - add adapta_fontsize.sh for users on legacy Cinnamon versions to control theme font characteristics

--- spice_specific/build-theme.sh
+++ spice_specific/build-theme.sh
@@ -18,6 +18,7 @@ mkdir "$BUILDDIR/Adapta";
 mkdir "$BUILDDIR/Adapta/gtk-2.0";
 mkdir "$BUILDDIR/Adapta/gtk-3.0";
 mkdir "$BUILDDIR/Adapta/gtk-3.22";
+mkdir "$BUILDDIR/Adapta/scripts";
 rsync -a -u  "$WORKDIR/shell/cinnamon"/ "$BUILDDIR/Adapta/cinnamon";
 ln -sr "$BUILDDIR/Adapta/cinnamon/assets/thumbnail.png" "$BUILDDIR/Adapta/cinnamon/thumbnail.png"
 rsync -a -u "$WORKDIR/gtk/asset/assets-gtk3"/ "$BUILDDIR/Adapta/assets";
@@ -37,13 +38,18 @@ cp "$WORKDIR/spice_specific/info.json" "$BUILDDIR/Adapta/info.json";
 cp "$WORKDIR/COPYING" "$BUILDDIR/Adapta/COPYING";
 cp "$WORKDIR/LICENSE_CC_BY_SA4" "$BUILDDIR/Adapta/LICENSE_CC_BY_SA4";
 cp "$WORKDIR/spice_specific/README.md" "$BUILDDIR/Adapta/README.md";
-cp "$WORKDIR/spice_specific/firefox_fix.sh" "$BUILDDIR/Adapta/firefox_fix.sh";
+cp "$WORKDIR/spice_specific/firefox_fix.sh" "$BUILDDIR/Adapta/scripts/firefox_fix.sh";
+cp "$WORKDIR/spice_specific/adapta_fontsize.sh" "$BUILDDIR/Adapta/scripts/adapta_fontsize.sh";
 cp "$WORKDIR/gtk/index.theme" "$BUILDDIR/Adapta/index.theme";
 cd "$BUILDDIR/Adapta";
 sed -i "s|#THEMENAME|Adapta|g" info.json;
 sed -i "s|#THEMENAME|Adapta|g" README.md;
 sed -i "s|#DESCRIPTION|A light theme based on Material Design Guidelines|g" info.json;
 sed -i "s|#DESCRIPTION|A light theme based on Material Design Guidelines|g" README.md;
+sed -i "s|#THEMENAME|Adapta|g" ./scripts/adapta_fontsize.sh;
+sed -i "s|#COLOR|#263238|g" ./scripts/adapta_fontsize.sh;
+sed -i "s|#THEMENAME|Adapta|g" ./cinnamon/cinnamon.css;
+sed -i "s|#FONTSIZE|System controlled|g" ./cinnamon/cinnamon.css;
 rsync -a -u "$BUILDDIR/Adapta"/ "$THEMEDIR/Adapta";
 
 #Adapta-Nokto
@@ -51,6 +57,7 @@ mkdir "$BUILDDIR/Adapta-Nokto";
 mkdir "$BUILDDIR/Adapta-Nokto/gtk-2.0";
 mkdir "$BUILDDIR/Adapta-Nokto/gtk-3.0";
 mkdir "$BUILDDIR/Adapta-Nokto/gtk-3.22";
+mkdir "$BUILDDIR/Adapta-Nokto/scripts";
 rsync -a -u  "$WORKDIR/shell/cinnamon-nokto"/ "$BUILDDIR/Adapta-Nokto/cinnamon";
 ln -sr "$BUILDDIR/Adapta-Nokto/cinnamon/assets/thumbnail.png" "$BUILDDIR/Adapta-Nokto/cinnamon/thumbnail.png"
 rsync -a -u "$WORKDIR/gtk/asset/assets-gtk3"/ "$BUILDDIR/Adapta-Nokto/assets";
@@ -70,13 +77,18 @@ cp "$WORKDIR/spice_specific/info.json" "$BUILDDIR/Adapta-Nokto/info.json";
 cp "$WORKDIR/COPYING" "$BUILDDIR/Adapta-Nokto/COPYING";
 cp "$WORKDIR/LICENSE_CC_BY_SA4" "$BUILDDIR/Adapta-Nokto/LICENSE_CC_BY_SA4";
 cp "$WORKDIR/spice_specific/README.md" "$BUILDDIR/Adapta-Nokto/README.md";
-cp "$WORKDIR/spice_specific/firefox_fix.sh" "$BUILDDIR/Adapta-Nokto/firefox_fix.sh";
+cp "$WORKDIR/spice_specific/firefox_fix.sh" "$BUILDDIR/Adapta-Nokto/scripts/firefox_fix.sh";
+cp "$WORKDIR/spice_specific/adapta_fontsize.sh" "$BUILDDIR/Adapta-Nokto/scripts/adapta_fontsize.sh";
 cp "$WORKDIR/gtk/index.theme-nokto" "$BUILDDIR/Adapta-Nokto/index.theme";
 cd "$BUILDDIR/Adapta-Nokto";
 sed -i "s|#THEMENAME|Adapta-Nokto|g" info.json;
 sed -i "s|#THEMENAME|Adapta-Nokto|g" README.md;
 sed -i "s|#DESCRIPTION|A dark theme based on Material Design Guidelines|g" info.json;
 sed -i "s|#DESCRIPTION|A dark theme based on Material Design Guidelines|g" README.md;
+sed -i "s|#THEMENAME|Adapta-Nokto|g" ./scripts/adapta_fontsize.sh;
+sed -i "s|#COLOR|#CFD8DC|g" ./scripts/adapta_fontsize.sh;
+sed -i "s|#THEMENAME|Adapta-Nokto|g" ./cinnamon/cinnamon.css;
+sed -i "s|#FONTSIZE|System controlled|g" ./cinnamon/cinnamon.css;
 rsync -a -u "$BUILDDIR/Adapta-Nokto"/ "$THEMEDIR/Adapta-Nokto";
 
 #Adapta-Eta
